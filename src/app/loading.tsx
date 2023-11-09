e7e3115b0d491982afa545481eb3d5f615f0f4fd
Jisan-Hasan <jisan.jh5@gmail.com>
2023-11-10 03:14:52 +0600

    Loading Component added

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+const Loading = () => {
+    return (
+        <div className="loader">
+            <div className="loading-infinity">
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
