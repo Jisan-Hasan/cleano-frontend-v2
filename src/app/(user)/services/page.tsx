7746b4fc06265df2a5d3c470ca50f9cbc8f79233
Jisan-Hasan <jisan.jh5@gmail.com>
2023-11-25 21:40:12 +0600

    Services Page added

--- src/app/(user)/services/page.tsx
+++ src/app/(user)/services/page.tsx
@@ -1,4 +1,5 @@
 import BreadCrumbComponent from "@/components/ui/BreadCrumb";
+import ServiceCard from "@/components/ui/ServiceCard";
 
 const items = [
     {
@@ -9,9 +10,32 @@ const items = [
 
 const ServicesPage = () => {
     return (
-        <div>
+        <div className="px-4">
             <BreadCrumbComponent items={items} title="Services" />
-            <h1>All Services here</h1>
+            <div className="py-10">
+                <div className="space-y-5 text-center">
+                    <h6 className="text-[#52B765] text-lg">[Our Services]</h6>
+                    <h1 className="text-8xl font-bold">
+                        Our Cleaning & Laundry Services
+                    </h1>
+                    <p className="max-w-md mx-auto leading-7">
+                        Our prices are simple and affordable which are easy on
+                        pocket in comparison with the high street prices
+                    </p>
+                </div>
+            </div>
+            <div className="mb-10">
+                <div className="max-w-screen-xl mx-auto px-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+                        <ServiceCard />
+                        <ServiceCard />
+                        <ServiceCard />
+                        <ServiceCard />
+                        <ServiceCard />
+                        <ServiceCard />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/ui/ServiceCard.tsx
+++ src/components/ui/ServiceCard.tsx
@@ -7,20 +7,23 @@ import dryCleanImage from "../../assets/images/dry-clean.png";
 const ServiceCard = () => {
     return (
         <Card
-            className="max-w-xs overflow-hidden rounded-md mr-3"
+            className="max-w-sm overflow-hidden rounded-md lg:mr-3"
             imgSrc={dryCleanImage.src}
             imgSize=""
         >
             <Card.Container className="p-6">
                 <Card.Container className="my-3">
                     <Card.Title>Men Nike Shoe</Card.Title>
+                    <div className="my-2 text-[#52B765] text-3xl font-semibold">
+                        $9.99
+                    </div>
                     <Card.Description>
                         This can save time and effort, and it can also help to
                         reduce the risk of errors.
                     </Card.Description>
                 </Card.Container>
                 <Card.Container className="flex items-center justify-between">
-                    <button className="border-2 border-[#52B765] text-[#52B765] hover:text-white py-2 px-2 rounded-md hover:bg-[#51B765] transition-all duration-700 flex items-center">
+                    <button className="border-2 border-[#52B765] text-[#52B765] hover:text-white py-2 px-4 rounded-md hover:bg-[#51B765] transition-all duration-700 flex items-center">
                         <span className="pr-2">
                             <ShoppingCart size={24} />
                         </span>
@@ -32,7 +35,6 @@ const ServiceCard = () => {
                             size={20}
                         />
                     </div>
-                    <Card.Title>$9.99</Card.Title>
                 </Card.Container>
             </Card.Container>
         </Card>
