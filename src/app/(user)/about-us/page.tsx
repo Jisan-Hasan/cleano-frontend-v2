0e2f3202529ad25aaec30c281de91a82a23cfefc
Jisan-Hasan <jisan.jh5@gmail.com>
2023-11-26 14:49:04 +0600

    About Us page added

--- src/app/(user)/about-us/page.tsx
+++ src/app/(user)/about-us/page.tsx
@@ -1,7 +1,28 @@
+import About from "@/components/About/About";
+import DataSection from "@/components/DataSection/DataSection";
+import ImportantInformationSection from "@/components/ImportantInformationSection/ImportantInformationSection";
+import NaturalCleaningProductSection from "@/components/NaturalCleaningProducts/NaturalCleaningProductSection";
+import BreadCrumbComponent from "@/components/ui/BreadCrumb";
+
+const items = [
+    {
+        label: "About Us",
+        link: "/about-us",
+    },
+];
+
 const AboutUsPage = () => {
     return (
         <div>
-            <h1>This is about us page</h1>
+            <BreadCrumbComponent items={items} title="About Us" />
+            <div>
+                <div>
+                    <ImportantInformationSection />
+                    <DataSection />
+                    <NaturalCleaningProductSection />
+                    <About />
+                </div>
+            </div>
         </div>
     );
 };
