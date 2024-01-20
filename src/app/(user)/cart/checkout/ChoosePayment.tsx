5dddd2dbdcb00463734ba0622e996252f0bbde3f
Jisan-Hasan <jisan.jh5@gmail.com>
2024-01-21 01:04:09 +0600

    bug-fix: revert back to previous keep-react version

--- src/app/(user)/cart/checkout/ChoosePayment.tsx
+++ src/app/(user)/cart/checkout/ChoosePayment.tsx
@@ -1,17 +1,40 @@
 "use client";
 import { Label, Radio } from "keep-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const ChoosePayment = () => {
+export const ChoosePayment = ({setValue,
+    setError,
+    clearErrors, errors}:any) => {
     const [selected, setSelected] = useState("");
 
     const handleOptionChange = (event: any) => {
         setSelected(event.target.value);
     };
 
+    useEffect(() => {
+        setValue("paymentMethod", selected != "" ? selected : null);
+
+        if (selected == "") {
+            setError("paymentMethod", {
+                type: "manual",
+                message: "Please select a payment method",
+            });
+        } else {
+            clearErrors("paymentMethod");
+        }
+    },[selected,setValue,setError, clearErrors])
+
     return (
         <fieldset className="flex flex-col gap-3" id="radio">
             <legend className="mb-3">Choose your Payment Method</legend>
+            {
+                errors && errors?.paymentMethod && (
+                    <div className="text-sm text-red-400">
+                        {(errors?.paymentMethod?.message as String) ||
+                            "*Please select a payment method"}
+                    </div>
+                )
+            }
             <div className="flex items-center gap-2">
                 <Radio
                     sizing="lg"
@@ -24,18 +47,7 @@ export const ChoosePayment = () => {
                 />
                 <Label htmlFor="card">Credit / Debit Card</Label>
             </div>
-            <div className="flex items-center gap-2">
-                <Radio
-                    sizing="lg"
-                    id="paypal"
-                    name="paymentMethod"
-                    selected={selected}
-                    onOptionChange={handleOptionChange}
-                    radioShape="square"
-                    value="paypal"
-                />
-                <Label htmlFor="paypal">Paypal</Label>
-            </div>
+            
             <div className="flex items-center gap-2">
                 <Radio
                     sizing="lg"
@@ -51,12 +63,25 @@ export const ChoosePayment = () => {
             <div className="flex items-center gap-2">
                 <Radio
                     sizing="lg"
+                    id="paypal"
+                    name="paymentMethod"
+                    selected={selected}
+                    onOptionChange={handleOptionChange}
+                    radioShape="square"
+                    value="paypal"
+                    disabled={true}
+                />
+                <Label htmlFor="paypal" disabled={true}>Paypal</Label>
+            </div>
+            <div className="flex items-center gap-2">
+                <Radio
+                    sizing="lg"
                     id="country-4"
                     name="paymentMethod"
                     selected={selected}
                     onOptionChange={handleOptionChange}
                     radioShape="square"
-                    value="China"
+                    value="ssl"
                     disabled={true}
                 />
                 <Label htmlFor="country-4" disabled={true}>
